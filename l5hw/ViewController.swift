cc098c42f82c6b1ed223aac5c954d72c326fea3c
Sanchikuuus <Sanchikuuus@ukr.net>
2019-06-06 21:54:50 +0300

    Initial Commit

---
+++ l5hw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  l5hw
+//
+//  Created by Sashko Shel on 6/6/19.
+//  Copyright © 2019 Sashko Shel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
